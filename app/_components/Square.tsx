4d790f2176c3f625761ee4c69d06c41379f00d82
Andrew <andrew@andrewsmith.org>
2024-02-19 05:21:32 -0500

    Remove boilerplate + add initial layout

---
+++ app/_components/Square.tsx
@@ -0,0 +1,5 @@
+export default function Square() {
+  return (
+    <div className="size-10 border-2 border-slate-600 hover:bg-slate-600"></div>
+  );
+}
---
+++ app/_components/Squares.tsx
@@ -0,0 +1,13 @@
+import Square from './Square';
+
+export default function Squares() {
+  return (
+    <div className="flex w-1/2 justify-center gap-2">
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+    </div>
+  );
+}
