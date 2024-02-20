17bd4397106ccd0cd9eeffb68d6dcf8f0b690f49
Andrew <andrew@andrewsmith.org>
2024-02-20 04:05:56 -0500

    Add bounce animation to squares

--- app/_components/Square.tsx
+++ app/_components/Square.tsx
@@ -1,5 +1,14 @@
+import { useState } from 'react';
+
 export default function Square() {
+  const [animated, setAnimated] = useState(false);
   return (
-    <div className="size-10 border-2 border-slate-600 hover:bg-slate-600"></div>
+    <div
+      className={`size-10 border-2 border-slate-600 hover:bg-slate-600 ${
+        animated ? 'animate-boxBounce' : ''
+      }`}
+      onMouseEnter={() => setAnimated(true)}
+      onAnimationEnd={() => setAnimated(false)}
+    ></div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      animation: {
+        boxBounce: 'boxBounce .5s ease-out',
+      },
+      keyframes: {
+        boxBounce: {
+          '0%': { transform: 'translateY(0)' },
+          '30%': { transform: 'translateY(-10px)' },
+          '50%, 100%': { transform: 'translateY(0)' },
+        },
       },
     },
   },
