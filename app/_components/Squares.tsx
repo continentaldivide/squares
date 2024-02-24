2c075f3d002363c59a08e081e25297cde3d8f88a
Andrew <andrew@andrewsmith.org>
2024-02-24 03:13:24 -0500

    Add button to randomize squares

--- app/_components/Squares.tsx
+++ app/_components/Squares.tsx
@@ -10,12 +10,14 @@ type Square = {
 
 export default function Squares() {
   const [squares, setSquares] = useState<Square[]>([]);
+
   useEffect(() => {
     if (squares.length < 5) {
       const newSquare = useGenerateSquare();
       setSquares([...squares, newSquare]);
     }
   }, [squares]);
+
   const squareComponents = squares.map((square, i) => {
     return (
       <Square
@@ -25,5 +27,16 @@ export default function Squares() {
       />
     );
   });
-  return <div className="flex justify-center gap-2">{squareComponents}</div>;
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="flex justify-center gap-2">{squareComponents}</div>
+      <button
+        className="w-48 h-10 text-lg rounded-lg bg-gray-500 hover:bg-gray-600 active:bg-gray-700 mt-4"
+        onClick={() => setSquares([])}
+      >
+        generate squares
+      </button>
+    </div>
+  );
 }
