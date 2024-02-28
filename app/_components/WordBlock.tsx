6008ef2ee27d8134a06d6f089ad44aaaa0286dd6
Andrew <andrew@andrewsmith.org>
2024-02-28 03:35:18 -0500

    Add reducer for state changes

--- app/_components/WordBlock.tsx
+++ app/_components/WordBlock.tsx
@@ -2,9 +2,9 @@ import { useGameStateContext } from '../_context/GameStateContext';
 
 export default function WordBlock() {
   const spaces = [];
-  const { wordBlockLength } = useGameStateContext();
+  const [gameState, gameStateDispatch] = useGameStateContext();
 
-  for (let i = 0; i < wordBlockLength; i++) {
+  for (let i = 0; i < gameState.wordBlockLength; i++) {
     const space = (
       <div
         className="size-12 border-2 border-teal-800"
@@ -13,6 +13,17 @@ export default function WordBlock() {
     );
     spaces.push(space);
   }
+  console.log(gameState);
 
-  return <div className="flex gap-2 min-h-12 mb-10">{spaces}</div>;
+  return (
+    <div className="flex gap-2 min-h-12 mb-10">
+      {spaces}
+      <button
+        className="w-48 h-10 text-lg rounded-lg bg-gray-500 hover:bg-gray-600 active:bg-gray-700 mt-4"
+        onClick={() => gameStateDispatch({ type: 'increase word block' })}
+      >
+        add a square
+      </button>
+    </div>
+  );
 }
--- app/_context/GameStateContext.tsx
+++ app/_context/GameStateContext.tsx
@@ -1,23 +1,31 @@
-import { createContext, useContext, ReactNode } from 'react';
+import { createContext, useContext, useReducer, ReactNode } from 'react';
 import GameStateType from '../_interfaces/GameState.interface';
 
-const GameStateContext = createContext<GameStateType>({
-  selectedSquares: [],
-  wordBlockLength: 3,
-});
+const GameStateContext = createContext<any>(null);
+
+const gameStateReducer = (state: GameStateType, action: any) => {
+  switch (action.type) {
+    case 'increase word block':
+      return {
+        ...state,
+        wordBlockLength: state.wordBlockLength + 1,
+      };
+    default:
+      return state;
+  }
+};
 
 export function GameStateContextProvider({
   children,
 }: {
   children: ReactNode;
 }) {
+  const [gameState, gameStateDispatch] = useReducer(gameStateReducer, {
+    selectedSquares: [],
+    wordBlockLength: 3,
+  });
   return (
-    <GameStateContext.Provider
-      value={{
-        selectedSquares: [],
-        wordBlockLength: 3,
-      }}
-    >
+    <GameStateContext.Provider value={[gameState, gameStateDispatch]}>
       {children}
     </GameStateContext.Provider>
   );
