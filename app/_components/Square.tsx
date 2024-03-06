3883bd8d8bc1d9c35c24fdd146e42fd78e76c701
Andrew <andrew@andrewsmith.org>
2024-03-06 04:00:07 -0500

    Reduce duplication in Square.tsx
    I'm still dubious that this is the best solution I can come up with for the problem of managing two different 'versions' of any possible square, but this will be much easier to change for now at least.

--- app/_components/Square.tsx
+++ app/_components/Square.tsx
@@ -15,10 +15,11 @@ export default function Square({
   const { gameState, gameStateDispatch } = useGameStateContext();
   const { backgroundColor, opacity } = square.styles;
   const letter = square.letter;
+  const baselineClasses = `flex justify-center items-center size-12 text-2xl ${backgroundColor} select-none`;
 
   const selectableSquare = (
     <div
-      className={`flex justify-center items-center size-12 text-2xl ${backgroundColor} ${opacity} select-none ${
+      className={`${baselineClasses} ${opacity} ${
         animated ? 'animate-boxBounce' : ''
       }`}
       onMouseEnter={() => setAnimated(true)}
@@ -37,13 +38,7 @@ export default function Square({
     </div>
   );
 
-  const wordBlockSquare = (
-    <div
-      className={`flex justify-center items-center size-12 text-2xl ${backgroundColor} select-none`}
-    >
-      {letter}
-    </div>
-  );
+  const wordBlockSquare = <div className={baselineClasses}>{letter}</div>;
 
   return insideWordBlock ? wordBlockSquare : selectableSquare;
 }
